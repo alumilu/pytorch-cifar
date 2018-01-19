2a2dae4e17a5397610edb9cb853ccf9f17c016a4
Hubert Lu <hubert.lu@acer.com>
2018-01-19 14:33:23 +0800

    add --train_batch_size and val_batch_size args

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--net', default='PreActResNet18', type=str, help='supported net: VGG, ResNeXt29_2x64d, ResNet18, GoogLeNet, DenseNet121, MobileNet, DPN92, SENet18')
+parser.add_argument('--train_batch_size', default=128, type=int, help='batch_size for trainning')
+parser.add_argument('--val_batch_size', default=100, type=int, help='batch_size for validating')
 args = parser.parse_args()
 
 use_cuda = torch.cuda.is_available()
@@ -43,10 +45,10 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.train_batch_size, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.val_batch_size, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
